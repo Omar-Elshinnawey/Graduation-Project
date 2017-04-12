8846add6621b8378e31df8660b8e2cc35e6d0b58
Omar Elshinnawey <el-shinnaweyom@hotmail.com>
2017-04-12 22:31:53 +0300

    added cfignore and modified documentation

--- api/order.api.js
+++ api/order.api.js
@@ -198,7 +198,7 @@ module.exports = function orderRouter(app) {
     //for all=============================================================
 
     /**
-     * @api {get} /orders/:orderId Get order details
+     * @api {get} /order/:orderId Get order details
      * @apiGroup Order
      * @apiDescription
      * Gets details of a specified order
