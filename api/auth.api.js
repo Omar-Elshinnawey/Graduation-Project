2abf53624004b0be4a18addc3c07a03f40202e12
Omar Elshinnawey <el-shinnaweyom@hotmail.com>
2017-04-23 17:48:31 +0300

    users can now change passwords

--- api/auth.api.js
+++ api/auth.api.js
@@ -17,25 +17,60 @@ module.exports = function authApi(app, authController) {
             .catch((err) => res.send(err));
     });
 
-    app.post('/auth/login',
-        passport.authenticate(
-            "local", {
-                failureRedirect: "/auth/failed"
-            }),
-        function(req, res) {
-            res.send(req.user.username);
-        });
+    app.post('/auth/login', function(req, res) {
+
+        passport.authenticate('local', function(err, user, info) {
+
+            if (err || !user)
+                return res.status(401).send(ERRORS.AUTH.INVALID_LOGIN);
+
+            req.logIn(user, function(err) {
+                if (err)
+                    return res.status(401).send(ERRORS.AUTH.INVALID_LOGIN);
+
+                var userobj = {};
+                userobj.username = user.username;
+                userobj.role = user.role;
+                userobj.email = user.email;
+                userobj.name = user.name;
+                userobj.address = user.address;
+                userobj.phone = user.phone;
+
+                res.send(userobj);
+            });
+        })(req, res);
+    });
+
+    app.post('/auth/change', function(req, res) {
+
+        passport.authenticate('local', function(err, user, info) {
+
+            if (err || !user)
+                return res.status(401).send(ERRORS.AUTH.INVALID_LOGIN);
+
+            user.setPassword(req.body.newPassword, function(err, result) {
+                if (err)
+                    return res.status(401).send(ERRORS.AUTH.INVALID_LOGIN);
+
+                user.save();
+
+                var userobj = {};
+                userobj.username = result.username;
+                userobj.role = result.role;
+                userobj.email = result.email;
+                userobj.name = result.name;
+                userobj.address = result.address;
+                userobj.phone = result.phone;
+                res.send(userobj);
+            });
+        })(req, res);
+
+    });
 
     app.get('/auth/logout', function(req, res) {
 
         req.logout();
         res.send(true);
-    });
-
-    app.get('/auth/failed', function(req, res) {
-
-        res.send(ERRORS.AUTH.INVALID_LOGIN);
-
     });
 
     app.get('/providers/:username', function(req, res) {
